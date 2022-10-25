3f390e337c7fb2d0331ed308194b6f7bfc6019a6
Uzair Iqbal <uzair.qq@outlook.com>
2022-10-25 22:52:51 +0500

    Creating Domain Models With Their Navigation Properties

---
+++ NZWalks.Api/Models/Domain/Region.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.Api.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+        public string Lat { get; set; }
+        public string Long { get; set; }
+        public long Population { get; set; }
+
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
