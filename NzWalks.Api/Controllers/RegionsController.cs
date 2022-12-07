78b2d77e50166ac3d94fad108ace99c5a48a6291
Uzair Iqbal <uzair.qq@outlook.com>
2022-12-07 20:54:03 +0500

    Created a Controller Called Regions, Api Created GetAllRegions

---
+++ NzWalks.Api/Controllers/RegionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NzWalks.Api.Data;
+
+namespace NzWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NzWalksDbContext _nzWalksDbContext;
+
+        
+        public RegionsController(NzWalksDbContext nzWalksDbContext)
+        {
+            _nzWalksDbContext = nzWalksDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await _nzWalksDbContext.Regions.ToListAsync();
+            return Ok(regions);
+        }
+    }
+}
