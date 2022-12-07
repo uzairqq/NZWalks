2fa9cf4d1c4922c46af1976f912e14c955e8b239
Uzair Iqbal <uzair.qq@outlook.com>
2022-12-07 20:05:17 +0500

    Added DbContext Class and Also Added Domain Models in that.

---
+++ NzWalks.Api/Data/NzWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalks.Api.Models.Domain;
+
+namespace NzWalks.Api.Data
+{
+    public class NzWalksDbContext : DbContext
+    {
+        public NzWalksDbContext(DbContextOptions<NzWalksDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+    }
+}
