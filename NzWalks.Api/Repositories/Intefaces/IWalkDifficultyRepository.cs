9fcb443b09833da3a854254e1d08212d4b5954e8
Uzair Iqbal <uzair.qq@outlook.com>
2022-12-22 18:34:13 +0500

    Extra

--- NzWalks.Api/Repositories/Intefaces/IWalkDifficultyRepository.cs
+++ NzWalks.Api/Repositories/Intefaces/IWalkDifficultyRepository.cs
@@ -5,14 +5,9 @@ namespace NzWalks.Api.Repositories.Intefaces
     public interface IWalkDifficultyRepository
     {
         Task<WalkDifficultyDto> AddAsync(AddUpdateWalkDifficultyDto addUpdateWalkDifficultyDto);
-
         Task<WalkDifficultyDto> UpdateAsync(Guid id, AddUpdateWalkDifficultyDto addUpdateWalkDifficultyDto);
-
         Task<IEnumerable<WalkDifficultyDto>> GetAllAsync();
         Task<WalkDifficultyDto> GetByIdAsync(Guid id);
-
         Task<WalkDifficultyDto> DeleteAsync(Guid id);
-
-
     }
 }
